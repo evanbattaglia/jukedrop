8f311f31060f6ac2704b57c859ea9cff8f52ed03
Evan Battaglia <evan@moz.com>
2016-07-31 23:00:39 -0700

    "dist" task

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 var path = require('path');
 
+var TARGET = process.env.npm_lifecycle_event;
+
+var BUILD_DIR_PRODUCTION = path.resolve(__dirname, 'dist/public/');
 var BUILD_DIR = path.resolve(__dirname, 'public/');
 var APP_DIR = path.resolve(__dirname, 'src/');
 
-var config = {
+var commonConfig = {
   entry: APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
@@ -18,7 +22,24 @@ var config = {
         loader: 'babel'
       }
     ]
-  }
+  },
 };
 
+var productionConfig = {
+  output: {
+    path: BUILD_DIR_PRODUCTION,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+  ],
+};
+
+var config = commonConfig;
+if (TARGET === 'dist') {
+  config = merge.smart(config, productionConfig);
+}
 module.exports = config;
