6065325b729ca4e3dda62bdf73821ebd9c60570e
Evan Battaglia <evan@moz.com>
2016-08-21 01:14:59 -0700

    wip

--- src/stores/PlaylistStore.js
+++ src/stores/PlaylistStore.js
@@ -1,131 +1,66 @@
-import {EventEmitter} from 'events';
-import ActionConstants from '../constants/ActionConstants';
-import ListStore from './ListStore';
+import alt from '../alt';
+import CurrentPlaylistActions from '../actions/CurrentPlaylistActions';
+import PlaylistsActions from '../actions/PlaylistsActions';
 
-class PlaylistStore extends ListStore {
-  // constructor not needed: starts out with
-  // undefined playlistName and empty items.
-  getPlaylistName() {
-    return this._playlistName;
+class PlaylistStore {
+  constructor() {
+    this.playlistName = null;
+    this.items = [];
+    this.bindListeners({
+      remove: CurrentPlaylistActions.REMOVE_ITEM,
+      add: CurrentPlaylistActions.ADD_ITEM,
+      onDeletePlaylist: PlaylistsActions.DELETE,
+      setPlaylist: PlaylistsActions.CHOOSE,
+    });
   }
 
-  // Protected Methods:
-  _setPlaylistName(name) {
-    this._playlistName = name;
-    this._loadFromSource(); // this also emits change
+  // Action handlers
+
+  add(item) {
+    if (!this._playlistName) return false;
+    if (~this.items.indexOf(item)) return false;
+    this.items.push(item);
+    this.save();
   }
 
-  _storageKey() {
+  remove(item) {
+    if (!this._playlistName) return false;
+    this.items = this.items.filter(i => i !== item);
+    this.save();
+  }
+
+  setPlaylist(name) {
+    this.playlistName = name;
+
+    // TODO: move to a source: LocalStorage.get(this.storageKey(), []); // [] is default
+    // LocalStorage.save(this.storageKey(), this.items);
+    const loadedJson = localStorage.getItem(this.storageKey());
+    if (loadedJson) {
+      this.items = JSON.parse(loadedJson).filter(x => x);
+    } else {
+      this.items = [];
+      this.save();
+    }
+  }
+
+  onDeletePlaylist(name) {
+    if (name === this._playlistName) {
+      this.playlistName = null;
+      this.items = [];
+    } else {
+      return false; // nothing changed
+    }
+  }
+
+  // Helpers
+
+  storageKey() {
     return 'jukedrop-playlist-' + this._playlistName; // TODO: sanitize?
   }
 
-  // Protected Methods Used by ListStore:
-  _loadJsonFromSource() {
-    if (this._playlistName) {
-      return localStorage.getItem(this._storageKey());
-    } else {
-      return null;
-    }
-  }
-
-  _saveJsonToSource(jsonStr) {
-    if (this._playlistName) {
-      localStorage.setItem(this._storageKey(), jsonStr);
-    }
-  }
-
-  _handleDispatch(payload) {
-    // TODO: I think this could be improved to remove all the if/else
-    if (payload.actionType === ActionConstants.CURRENT_PLAYLIST_REMOVE_ITEM) {
-      this._remove(payload.path);
-    } else if (payload.actionType === ActionConstants.CURRENT_PLAYLIST_ADD_ITEM) {
-      this._add(payload.path);
-    } else if (payload.actionType === ActionConstants.PLAYLISTS_DELETE) {
-      if (payload.name === this._playlistName) this._setPlaylistName(null);
-    } else if (payload.actionType === ActionConstants.PLAYLISTS_CHOOSE) {
-      this._setPlaylistName(payload.name);
-    }
+  save() {
+    localStorage.setItem(this.storageKey(), JSON.stringify(this._items));
   }
 }
 
-
-export default new PlaylistStore();
-import {EventEmitter} from 'events';
-import Dispatcher from '../Dispatcher';
-
-/**
- * Base class for a store that is a list of items (for now, strings).
- * List can not have duplicates.
- * Each item is identified by its string (for removing and checking of duplicates)
- *
- * Subclasses should implement:
- *   _handleDispatch(payload)
- *   _loadJsonFromSource() (returns string)
- *   _saveJsonToSource(jsonStr)
- *
- * Calls change listeners with no parameters when change occurs.
- *
- * Public Methods:
- *   addChangeListener(callback)
- *   removeChangeListener(callback)
- *   getItems()
- */
-
-const CHANGE_EVENT = 'change';
-
-class ListStore extends EventEmitter {
-  // PUBLIC METHODS
-  addChangeListener(callback) {
-    this.on(CHANGE_EVENT, callback);
-  }
-
-  removeChangeListener(callback) {
-    this.removeListener(CHANGE_EVENT, callback);
-  }
-
-  getItems() {
-    return this._items;
-  }
-
-  // CONSTRUCTOR
-  constructor(storageKey) {
-    super();
-    this.dispatcherIndex = Dispatcher.register(this._handleDispatch.bind(this));
-    this._items = [];
-    this._loadFromSource();
-  }
-
-  // PRIVATE METHODS
-  _emitChange() {
-    this.emit(CHANGE_EVENT);
-  }
-
-  _loadFromSource() {
-    const loadedJson = this._loadJsonFromSource();
-    if (loadedJson) {
-      this._items = JSON.parse(loadedJson).filter(x => x);
-      this._emitChange();
-    } else {
-      this._items = [];
-      this._saveAndEmitChange();
-    }
-  }
-
-  _saveAndEmitChange() {
-    this._saveJsonToSource(JSON.stringify(this._items));
-    this._emitChange();
-  }
-
-  _add(item) {
-    if (~this._items.indexOf(item)) return;
-    this._items.push(item);
-    this._saveAndEmitChange();
-  }
-
-  _remove(item) {
-    this._items = this._items.filter(i => i !== item);
-    this._saveAndEmitChange();
-  }
-}
-
-export default ListStore;
+export default alt.createStore(PlaylistStore, 'PlaylistStore');
--- src/stores/PlaylistsListStore.js
+++ src/stores/PlaylistsListStore.js
@@ -1,112 +1,35 @@
-import {EventEmitter} from 'events';
-import ActionConstants from '../constants/ActionConstants';
-import ListStore from './ListStore';
+import alt from '../alt';
+import PlaylistsActions from '../actions/PlaylistsActions';
 
-class PlaylistsListStore extends ListStore {
-  _storageKey() {
-    return 'jukedrop-playlists-';
-  }
+const STORAGE_KEY =  'jukedrop-playlists-';
 
-  // TODO: could dedup and put in ListStore
-  // Protected Methods Used by ListStore:
-  _loadJsonFromSource() {
-    return localStorage.getItem(this._storageKey());
-  }
-
-  _saveJsonToSource(jsonStr) {
-    localStorage.setItem(this._storageKey(), jsonStr);
-  }
-
-  _handleDispatch(payload) {
-    // TODO: I think this could be improved to remove all the if/else
-    if (payload.actionType === ActionConstants.PLAYLISTS_ADD) {
-      this._add(payload.name);
-    } else if (payload.actionType === ActionConstants.PLAYLISTS_DELETE) {
-      this._remove(payload.name);
-    } else if (payload.actionType === ActionConstants.PLAYLISTS_CHOOSE) {
-      // TODO: could highlight, etc. delete would have to check if chosen one is being deleted.
-    }
-  }
-}
-
-
-export default new PlaylistsListStore();
-import {EventEmitter} from 'events';
-import Dispatcher from '../Dispatcher';
-
-/**
- * Base class for a store that is a list of items (for now, strings).
- * List can not have duplicates.
- * Each item is identified by its string (for removing and checking of duplicates)
- *
- * Subclasses should implement:
- *   _handleDispatch(payload)
- *   _loadJsonFromSource() (returns string)
- *   _saveJsonToSource(jsonStr)
- *
- * Calls change listeners with no parameters when change occurs.
- *
- * Public Methods:
- *   addChangeListener(callback)
- *   removeChangeListener(callback)
- *   getItems()
- */
-
-const CHANGE_EVENT = 'change';
-
-class ListStore extends EventEmitter {
-  // PUBLIC METHODS
-  addChangeListener(callback) {
-    this.on(CHANGE_EVENT, callback);
-  }
-
-  removeChangeListener(callback) {
-    this.removeListener(CHANGE_EVENT, callback);
-  }
-
-  getItems() {
-    return this._items;
-  }
-
+class PlaylistsListStore {
   // CONSTRUCTOR
-  constructor(storageKey) {
-    super();
-    this.dispatcherIndex = Dispatcher.register(this._handleDispatch.bind(this));
-    this._items = [];
-    this._loadFromSource();
+  constructor() {
+    this.bindActions(PlaylistsActions);
+
+    // TODO move to a source, see PlaylistStore
+    const loadedJson = localStorage.getItem(STORAGE_KEY) || '[]';
+    this.items = JSON.parse(loadedJson).filter(x => x);
+    this.save();
   }
 
-  // PRIVATE METHODS
-  _emitChange() {
-    this.emit(CHANGE_EVENT);
+  onAddPlaylist() {
+    if (~this.items.indexOf(item)) return false;
+    this.items.push(item);
+    this.save();
+  }
+  onRemovePlaylist() {
+    this.items = this.items.filter(i => i !== item);
+    this.save();
+  }
+  onChoosePlaylist() {
+    return false; // TODO: could highlight, etc. delete would have to check if chosen one is being deleted.
   }
 
-  _loadFromSource() {
-    const loadedJson = this._loadJsonFromSource();
-    if (loadedJson) {
-      this._items = JSON.parse(loadedJson).filter(x => x);
-      this._emitChange();
-    } else {
-      this._items = [];
-      this._saveAndEmitChange();
-    }
-  }
-
-  _saveAndEmitChange() {
-    this._saveJsonToSource(JSON.stringify(this._items));
-    this._emitChange();
-  }
-
-  _add(item) {
-    if (~this._items.indexOf(item)) return;
-    this._items.push(item);
-    this._saveAndEmitChange();
-  }
-
-  _remove(item) {
-    this._items = this._items.filter(i => i !== item);
-    this._saveAndEmitChange();
+  save() {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(this.items));
   }
 }
 
-export default ListStore;
+export default alt.createStore(PlaylistsListStore, 'PlaylistsListStore');
